b63dd1d23e4a72013b1a8abb085773f25811a396
ryantsang92 <ryantsang92@gmail.com>
2021-02-25 21:52:23 -0500

    IT'S ALIIIIIIIIIIVE!!!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./viva-logo.png";
 import "./App.css";
 import "./VivaLogo.css";
+import Box from "@material-ui/core/Box";
 import PillBox from "./components/pill-box";
 import SearchBar from "./components/search-bar";
 import Footer from "./components/footer";
-import Map from "./components/map";
+import MapContainer from "./components/map";
 
 const App = () => {
-  // mock data
+
+  // mock data section ////////////////////////////
   const buttonTitles = [
     { name: "#TreatYoSelf", value: "1" },
     { name: "#GreatOutdoors", value: "2" },
@@ -20,19 +22,51 @@ const App = () => {
     { name: "#PicturePerfect", value: "8" },
   ];
 
+  const places = [
+    {
+      name: "Improv Asylum",
+      title: "Improv Asylum",
+      lat: 42.3631,
+      lng: -71.0554,
+      id: 1,
+    },
+    {
+      name: "Limelight",
+      title: "Limelight",
+      lat: 42.3513581,
+      lng: -71.064757,
+      id: 2,
+    },
+    {
+      name: "Paint Nite",
+      title: "Paint Nite",
+      lat: 42.383486,
+      lng: -71.109816,
+      id: 3,
+    },
+    {
+      name: "A4cade",
+      title: "A4cade",
+      lat: 42.362142,
+      lng: -71.098406,
+      id: 4,
+    },
+  ];
+  // end mock data section ////////////////////////
+
   return (
     <div className="App">
       <header>
         <img src={logo} alt="logo" className="VivaLogo" />
-        <p>
+        <Box mb={2}>
           <SearchBar />
           <PillBox buttonTitles={buttonTitles} />
-        </p>
+        </Box>
       </header>
-      <Map isMarkerShown />
+      <MapContainer places={places}/>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/map.js
+++ src/components/map.js
@@ -1,59 +1,57 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import Box from "@material-ui/core/Box";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 import PropTypes from "prop-types";
 
-const containerStyle = {
-  width: "400px",
-  height: "400px",
-};
-
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 42.3601,
+  lng: -71.0589,
 };
 
+// to-do: fix map styling and positioning
 const mapStyles = {
-  width: "100%",
-  height: "100%",
+  marginLeft: "30px",
+  width: "95%",
+  height: "95%",
 };
 
-// const MapContainer = () => {
-//   return (
-//     <Map
-//       google={this.props.google}
-//       zoom={14}
-//       style={mapStyles}
-//       initialCenter={{
-//         lat: -1.2884,
-//         lng: 36.8233,
-//       }}
-//     />
-//   );
-// };
+const MapContainer = ({ loaded, google, places }) => {
+  return (
+    <>
+      {!loaded ? (
+        <div>Loading...</div>
+      ) : (
+        <Box mx="2">
+          <Map
+            google={google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={center}
+          >
+            {places.map((place) => {
+              return (
+                <Marker
+                  key={place.id}
+                  position={{ lat: place.lat, lng: place.lng }}
+                />
+              );
+            })}
+          </Map>
+        </Box>
+      )}
+    </>
+  );
+};
 
-const MapContainer = (props) => {
-    console.log('props.google');
-    console.log(props.google);
-    return (
-      <Map
-        google={props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233,
-        }}
-      />
-    );
-}
+MapContainer.propTypes = {
+  google: PropTypes.object,
+  places: PropTypes.array,
+};
 
-// Map.propTypes = {
-//   isMarkerShown: PropTypes.bool,
-// };
-
-// Map.defaultProps = {
-//   isMarkerShown: false,
-// };
+MapContainer.defaultProps = {
+  google: {},
+  places: [],
+};
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyDxUpiuJMJwjPvtBeuXJyRcm66jqEx38kA",
