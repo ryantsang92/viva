d252e5c8e9ef99db746060fdfb376eae1f4fd692
ryantsang92 <ryantsang92@gmail.com>
2021-03-23 17:26:20 -0400

    logo changes

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@
 */
 
 import React from "react";
-import logo from "../viva-logo.png";
+import logo from "../viva-logo-transparent.png";
 import "../VivaLogo.css";
 import { Box, Typography } from "@material-ui/core";
 import PillBoxContainer from "./pill-box-container";
@@ -14,7 +14,7 @@ import SearchBar from "./search-bar";
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt="logo" className="VivaLogo" />
+      <img src={logo} alt="VIVA" className="VivaLogo" />
       <Box mt={2} mb={2}>
         <Typography align="center">Designed to get you out there</Typography>
       </Box>
