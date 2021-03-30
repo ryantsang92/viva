f08ffac9907d0ecb735c7c236fb9f4bf3ffe7c98
ryantsang92 <ryantsang92@gmail.com>
2021-03-29 23:30:51 -0400

    it's working now

--- src/components/map/map-container.js
+++ src/components/map/map-container.js
@@ -6,19 +6,23 @@
 
 import { connect } from "react-redux";
 import { fetchLocations } from "../../actions/location-actions";
-import { selectLocationData } from "../../selectors/location-selectors";
+import { selectSelectedHashtag } from "../../selectors/hashtag-selectors";
+import { selectLocationData, selectLocationsByHashtag } from "../../selectors/location-selectors";
 import Map from "./map";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  const selectedHashtag = selectSelectedHashtag(state);
   return {
-    locations: selectLocationData(state).locations,
+    locations: selectedHashtag
+      ? selectLocationsByHashtag(state, selectedHashtag)
+      : selectLocationData(state).locations,
     // to-do: find a way to do optional chaining
     // hashtags: selectLocationData(state)?.locations,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    fetchLocations: () => dispatch(fetchLocations()),
+  fetchLocations: () => dispatch(fetchLocations()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
--- src/selectors/location-selectors.js
+++ src/selectors/location-selectors.js
@@ -4,6 +4,8 @@
   author: Ryan Tsang <ryan@vivatheapp.com>
 */
 
+import { selectVideosByHashtag } from "./video-selectors";
+
 export const selectLocationData = (state) => {
   return state ? state.locationData : null;
 };
@@ -14,4 +16,24 @@ export const selectLocationIsFetching = (state) => {
 
 export const selectLocationError = (state) => {
   return selectLocationData(state).error;
+};
+
+export const selectLocationsByHashtag = (state, hashtag) => {
+  const selectedVideos = selectVideosByHashtag(state, hashtag);
+  let combinedHashtags = [];
+  let locationIds = [];
+  if (selectedVideos) {
+    selectedVideos.forEach((video) => combinedHashtags.push(video.location_id));
+    locationIds = [].concat.apply([], combinedHashtags);
+  }
+  console.log(
+    state.locationData.locations.filter((location) =>
+      locationIds.includes(location.id)
+    )
+  );
+  return state
+    ? state.locationData.locations.filter((location) =>
+        locationIds.includes(location.id)
+      )
+    : null;
 };
