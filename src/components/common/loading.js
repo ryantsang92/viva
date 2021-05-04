bab810666f5b2dff09c243c01a552579d05c2750
ryantsang92 <ryantsang92@gmail.com>
2021-05-03 20:42:39 -0400

    Loading component fix

--- src/components/common/loading.js
+++ src/components/common/loading.js
@@ -8,7 +8,16 @@ import React from "react";
 import loading from "../../assets/loading.gif";
 
 const Loading = () => {
-  return <img src={loading} alt="loading" />;
+  return (
+    <img
+      src={loading}
+      alt="loading"
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
 };
 
 export default Loading;
