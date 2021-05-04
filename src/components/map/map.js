17d5630aebc202d9fd7ba23b81a3fab4734bd107
ryantsang92 <ryantsang92@gmail.com>
2021-05-03 20:37:16 -0400

    Map loading container fix

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -14,6 +14,8 @@ import Loading from "../common/loading";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
+const loading = (props) => <Loading />;
+
 const useStyles = makeStyles({
   infoWindow: {
     textAlign: "left",
@@ -109,54 +111,54 @@ const Map = ({
 
   const mapStyle = [
     {
-      "featureType": "poi.business",
-      "stylers": [
+      featureType: "poi.business",
+      stylers: [
         {
-          "visibility": "off"
-        }
-      ]
+          visibility: "off",
+        },
+      ],
     },
     {
-      "featureType": "poi.government",
-      "stylers": [
+      featureType: "poi.government",
+      stylers: [
         {
-          "visibility": "off"
-        }
-      ]
+          visibility: "off",
+        },
+      ],
     },
     {
-      "featureType": "poi.medical",
-      "stylers": [
+      featureType: "poi.medical",
+      stylers: [
         {
-          "visibility": "off"
-        }
-      ]
+          visibility: "off",
+        },
+      ],
     },
     {
-      "featureType": "poi.place_of_worship",
-      "stylers": [
+      featureType: "poi.place_of_worship",
+      stylers: [
         {
-          "visibility": "off"
-        }
-      ]
+          visibility: "off",
+        },
+      ],
     },
     {
-      "featureType": "poi.school",
-      "stylers": [
+      featureType: "poi.school",
+      stylers: [
         {
-          "visibility": "off"
-        }
-      ]
+          visibility: "off",
+        },
+      ],
     },
     {
-      "featureType": "poi.sports_complex",
-      "stylers": [
+      featureType: "poi.sports_complex",
+      stylers: [
         {
-          "visibility": "simplified"
-        }
-      ]
-    }
-  ]
+          visibility: "simplified",
+        },
+      ],
+    },
+  ];
 
   const mapLoaded = (mapProps, map) => {
     map.setOptions({
@@ -165,94 +167,88 @@ const Map = ({
   };
 
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <Box mr={2} className={classes.test}>
-          <GoogleMap
-            google={google}
-            zoom={zoom}
-            mapTypeControl={false}
-            scaleControl={false}
-            streetViewControl={false}
-            fullscreenControl={false}
-            zoomControl
-            containerStyle={mapContainerStyle}
-            style={style}
-            resetBoundsOnResize
-            center={center}
-            initialCenter={center}
-            zoomControlOptions={{
-              position: google.maps.ControlPosition.TOP_RIGHT,
-            }}
-            onReady={(mapProps, map) => mapLoaded(mapProps, map)}
-          >
-            {locations &&
-              locations.map((location) => {
-                return (
-                  <Marker
-                    className={classes.marker}
-                    name={location.id}
-                    key={location.id}
-                    markerData={location}
-                    position={{
-                      lat: parseFloat(location.lat),
-                      lng: parseFloat(location.lng),
-                    }}
-                    onClick={onMarkerClick}
-                    icon={{
-                      url:
-                        selectedLocation && selectedLocation.id === location.id
-                          ? MapPinSelected
-                          : MapPinDefault,
-                      scaledSize: new google.maps.Size(30, 36),
-                    }}
-                  />
-                );
-              })}
-            {selectedLocation && (
-              <InfoWindowEx
+    <Box mr={2} className={classes.test}>
+      <GoogleMap
+        google={google}
+        zoom={zoom}
+        mapTypeControl={false}
+        scaleControl={false}
+        streetViewControl={false}
+        fullscreenControl={false}
+        zoomControl
+        containerStyle={mapContainerStyle}
+        style={style}
+        resetBoundsOnResize
+        center={center}
+        initialCenter={center}
+        zoomControlOptions={{
+          position: google.maps.ControlPosition.TOP_RIGHT,
+        }}
+        onReady={(mapProps, map) => mapLoaded(mapProps, map)}
+      >
+        {locations &&
+          locations.map((location) => {
+            return (
+              <Marker
+                className={classes.marker}
+                name={location.id}
+                key={location.id}
+                markerData={location}
                 position={{
-                  lat: parseFloat(selectedLocation.lat),
-                  lng: parseFloat(selectedLocation.lng),
+                  lat: parseFloat(location.lat),
+                  lng: parseFloat(location.lng),
                 }}
-                visible={infoOpen}
-                onClose={onInfoWindowClose}
-                pixelOffset={new google.maps.Size(0, -35)}
-              >
-                <Box className={classes.infoWindow}>
-                  <h6>{selectedLocation.name}</h6>
-                  <Typography fontFamily="Arial">
-                    {selectedLocation.address_full}
-                  </Typography>
-                  <Typography>
-                    <a
-                      href={selectedLocation.website}
-                      target={selectedLocation.website}
-                    >
-                      {
-                        selectedLocation.website
-                          .replace(/^(?:https?:\/\/)?(?:www\.)?/i, "")
-                          .split("/")[0]
-                      }
-                    </a>
-                  </Typography>
-                  <Box pt={1} pb={1}>
-                    <Divider />
-                  </Box>
-                  <Typography>
-                    <a href="/#" onClick={onRelatedVideosClick}>
-                      See related videos
-                    </a>
-                  </Typography>
-                </Box>
-              </InfoWindowEx>
-            )}
-          </GoogleMap>
-        </Box>
-      )}
-    </>
+                onClick={onMarkerClick}
+                icon={{
+                  url:
+                    selectedLocation && selectedLocation.id === location.id
+                      ? MapPinSelected
+                      : MapPinDefault,
+                  scaledSize: new google.maps.Size(30, 36),
+                }}
+              />
+            );
+          })}
+        {selectedLocation && (
+          <InfoWindowEx
+            position={{
+              lat: parseFloat(selectedLocation.lat),
+              lng: parseFloat(selectedLocation.lng),
+            }}
+            visible={infoOpen}
+            onClose={onInfoWindowClose}
+            pixelOffset={new google.maps.Size(0, -35)}
+          >
+            <Box className={classes.infoWindow}>
+              <h6>{selectedLocation.name}</h6>
+              <Typography fontFamily="Arial">
+                {selectedLocation.address_full}
+              </Typography>
+              <Typography>
+                <a
+                  href={selectedLocation.website}
+                  target={selectedLocation.website}
+                >
+                  {
+                    selectedLocation.website
+                      .replace(/^(?:https?:\/\/)?(?:www\.)?/i, "")
+                      .split("/")[0]
+                  }
+                </a>
+              </Typography>
+              <Box pt={1} pb={1}>
+                <Divider />
+              </Box>
+              <Typography>
+                <a href="/#" onClick={onRelatedVideosClick}>
+                  See related videos
+                </a>
+              </Typography>
+            </Box>
+          </InfoWindowEx>
+        )}
+      </GoogleMap>
+    </Box>
   );
 };
 
@@ -284,5 +280,5 @@ Map.defaultProps = {
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyDxUpiuJMJwjPvtBeuXJyRcm66jqEx38kA",
-  // LoadingContainer: <Loading />,
+  LoadingContainer: loading,
 })(Map);
