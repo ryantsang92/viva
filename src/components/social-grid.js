3876c3418f84de5d4d86090caeb3d0ede4cf885f
ryantsang92 <ryantsang92@gmail.com>
2021-06-06 22:57:22 -0400

    Social grid change

--- src/components/social-grid.js
+++ src/components/social-grid.js
@@ -5,7 +5,7 @@
 */
 
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 import { SocialIcon } from "./social-icon";
 import { socialURLs } from "../app-constants";
 import instagramIcon from "../assets/instagram-logo.svg";
@@ -15,16 +15,19 @@ import tiktokIcon from "../assets/tiktok-logo.svg";
 const { INSTAGRAM, TWITTER, TIKTOK } = socialURLs;
 
 const SocialGrid = () => {
-
   return (
-    <>
+    <Box
+      display="flex"
+      flexDirection="row"
+      flexWrap="nowrap"
+      alignItems="center"
+    >
+      <Box flexGrow={1} pr={1}>
+        <Typography noWrap>Follow us: </Typography>
+      </Box>
       <Grid container justify="flex-end">
         <Grid item>
-          <SocialIcon
-            url={INSTAGRAM}
-            icon={instagramIcon}
-            hw={30}
-          />
+          <SocialIcon url={INSTAGRAM} icon={instagramIcon} hw={30} />
         </Grid>
         <Grid item>
           <SocialIcon url={TWITTER} icon={twitterIcon} hw={30} />
@@ -33,7 +36,7 @@ const SocialGrid = () => {
           <SocialIcon url={TIKTOK} icon={tiktokIcon} hw={30} />
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };
 
